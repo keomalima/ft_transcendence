5c3cd2c051121aae1ef7c2ee3d424da4e37c2f66
Zhenyu Jiang <zjiang@z1r10p4.42lyon.fr>
2026-01-14 10:11:02 +0100

    new branch for game invitation. set up the env for dev mode

--- frontend/src/pages/LiveChat.ts
+++ frontend/src/pages/LiveChat.ts
@@ -255,6 +255,7 @@ function setupLiveChatEventListeners(ctx: AppContext) {
 }
 
 async function renderChatBox(friend: any, ctx: AppContext) {
+	console.log("hi", ctx.gameStore.get());// check if show the button for invite
 	const chatRight = document.getElementById('chat-right');
 	const chatEmpty = document.getElementById('chat-empty');
 	if (!chatRight || !chatEmpty) return;
