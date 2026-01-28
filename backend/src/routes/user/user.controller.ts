a5cfd10991064d364627f9615378513bdcda7d6d
Keoma Ricci De Lima <kricci-d@z2r9p2.42lyon.fr>
2026-01-28 11:04:04 +0100

    minor fixes

--- backend/src/routes/user/user.controller.ts
+++ backend/src/routes/user/user.controller.ts
@@ -65,7 +65,7 @@ async function protectedRouteHandler(request: FastifyRequest, reply: FastifyRepl
 async function validateSessionAuth ( request: FastifyRequest, reply: FastifyReply) {
 	if (request.user)
 		return reply.code(200).send(request.user);
-	else {
+	else
 		return reply.code(401).send({ message: "Unauthorized" });
 }
 
@@ -154,7 +154,7 @@ async function logoutHandler(request: FastifyRequest, reply: FastifyReply) {
 	try {
 		await userService.logoutUser(request.server.prisma, request.user!.id);
 		reply.clearCookie('sessionId', { path: '/' });
-		reply.code(204).send()
+		reply.code(204).send();
 	} catch (error: unknown) {
 		if (error instanceof Error)
 			return reply.code(401).send({ message: error.message });
